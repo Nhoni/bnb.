8cee653190e6e457129c7e9720163caef0443045
Nhoni <nhmbemba@gmail.com>
2024-01-19 19:37:17 +0100

    routes roms

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -7,15 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/r')] //prefix all room routes with /r
+#[Route('/r')] // prefix all room routes with /r
 class RoomController extends AbstractController
 {
     #[Route('/', name: 'app_room')]
-    public function index(RoomRepository $roomRepository
+    public function index(
+        RoomRepository $roomRepository
     ): Response
     {
         return $this->render('room/index.html.twig', [
-            'rooms' => $roomRepository->findAll()
+            'rooms' => $roomRepository->findAll(),
+            'hostRooms' => $roomRepository->findBy(
+                ['host' => $this->getUser()]
+                )
         ]);
     }
 }
